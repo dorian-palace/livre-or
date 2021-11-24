49f5abf6055114a6390b63ee2b125e8645c2089d
dorian-palace <dorian.palace@laplateforme.io>
2021-11-24 13:38:11 +0100

    livre

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="livreor.css">
+    <title>Livre d'or</title>
+</head>
+<body>
+    <header>
+        <ul class="navbar">
+            <div class="lianav">
+            <li class="linav"><a class="anav" href="index.php">Accueil</a></li>
+            <li class="linav"><a class="anav" href="inscription.php">Inscription</a></li>
+            <li class="linav"><a class="anav" href="connexion.php">Connexion</a></li>
+            <li class="linav"><a class="anav" href="profil.php">Profil</a></li>
+            <li class="linav"><a class="anav" href="livreor.php">Livre d'or</a></li>
+            <li class="linav"><a class="anav" href="commentaire.php">Commentaire</a></li>
+            </div>
+        </ul>
+    </header>
+    <main>
+        
+    </main>
+
+    <footer>
+        <div class="afooter">
+        <a href="index.php">Accueil</a>
+        <a href="inscription.php">Inscription</a>
+        <a href="connexion.php">Connexion</a>
+        <a href="profil.php">Profil</a>
+        <a href="lvireor.php">Livre d'or</a>
+        <a href="commentaire.php">Commentaire</a>
+        </div>
+    </footer>
+</body>
+</html>
